eb47bc3a7a45682502bda33a1f7e90160c47e42f
lizard.liang <lizard.liang@watchsoft.com.tw>
2024-08-27 13:37:17 +0800

    20240827

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -39,7 +39,7 @@ function ls-force {
 Set-Alias -Name la -Value ls-force 
 
 $scoopDir = "$env:USERPROFILE\scoop"
-$lessPath = "$scoopDir\apps\git\2.45.2\usr\bin\less.exe"
+$lessPath = "$scoopDir\apps\git\current\usr\bin\less.exe"
 
 Set-Alias g git
 Set-Alias gcz 'C:\Program Files\Git\cmd\git.exe cz'
@@ -98,6 +98,10 @@ function Change-Directory {
     $osc7 = "$([char]27)]7;file://$($cwd.ProviderPath)$([char]7)"
     [console]::OutputEncoding = [System.Text.Encoding]::UTF8
     Write-Host -NoNewline $osc7
+
+    $osc99 = "$([char]27)]99;file://$($cwd.ProviderPath)$([char]9)"
+    [console]::OutputEncoding = [System.Text.Encoding]::UTF8
+    Write-Host -NoNewline $osc99
 }
 
 Set-Alias -Name clang -Value clang64
@@ -135,136 +139,9 @@ function wsl-tree {
 
 Set-Alias -Name tree -Value wsl-tree
 
-# =============================================================================
-#
-# Utility functions for zoxide.
-#
+. $HOME\.config\powershell\env_profile.ps1
 
-# Call zoxide binary, returning the output as UTF-8.
-function global:__zoxide_bin {
-    $encoding = [Console]::OutputEncoding
-    try {
-        [Console]::OutputEncoding = [System.Text.Utf8Encoding]::new()
-        $result = zoxide @args
-        return $result
-    } finally {
-        [Console]::OutputEncoding = $encoding
-    }
-}
-
-# pwd based on zoxide's format.
-function global:__zoxide_pwd {
-    $cwd = Get-Location
-    if ($cwd.Provider.Name -eq "FileSystem") {
-        $cwd.ProviderPath
-    }
-}
-
-# cd + custom logic based on the value of _ZO_ECHO.
-function global:__zoxide_cd($dir, $literal) {
-    $dir = if ($literal) {
-        Set-Location -LiteralPath $dir -Passthru -ErrorAction Stop
-    } else {
-        if ($dir -eq '-' -and ($PSVersionTable.PSVersion -lt 6.1)) {
-            Write-Error "cd - is not supported below PowerShell 6.1. Please upgrade your version of PowerShell."
-        }
-        elseif ($dir -eq '+' -and ($PSVersionTable.PSVersion -lt 6.2)) {
-            Write-Error "cd + is not supported below PowerShell 6.2. Please upgrade your version of PowerShell."
-        }
-        else {
-            Set-Location -Path $dir -Passthru -ErrorAction Stop
-        }
-    }
-}
-
-# =============================================================================
-#
-# Hook configuration for zoxide.
-#
-
-# Hook to add new entries to the database.
-$global:__zoxide_oldpwd = __zoxide_pwd
-function global:__zoxide_hook {
-    $result = __zoxide_pwd
-    if ($result -ne $global:__zoxide_oldpwd) {
-        if ($null -ne $result) {
-            zoxide add -- $result
-        }
-        $global:__zoxide_oldpwd = $result
-    }
-}
-
-# Initialize hook.
-$global:__zoxide_hooked = (Get-Variable __zoxide_hooked -ErrorAction SilentlyContinue -ValueOnly)
-if ($global:__zoxide_hooked -ne 1) {
-    $global:__zoxide_hooked = 1
-    $global:__zoxide_prompt_old = $function:prompt
-
-    function global:prompt {
-        if ($null -ne $__zoxide_prompt_old) {
-            & $__zoxide_prompt_old
-        }
-        $null = __zoxide_hook
-    }
-}
-
-# =============================================================================
-#
-# When using zoxide with --no-cmd, alias these internal functions as desired.
-#
-
-# Jump to a directory using only keywords.
-function global:__zoxide_z {
-    if ($args.Length -eq 0) {
-        __zoxide_cd ~ $true
-    }
-    elseif ($args.Length -eq 1 -and ($args[0] -eq '-' -or $args[0] -eq '+')) {
-        __zoxide_cd $args[0] $false
-    }
-    elseif ($args.Length -eq 1 -and (Test-Path $args[0] -PathType Container)) {
-        __zoxide_cd $args[0] $true
-    }
-    else {
-        $result = __zoxide_pwd
-        if ($null -ne $result) {
-            $result = __zoxide_bin query --exclude $result -- @args
-        }
-        else {
-            $result = __zoxide_bin query -- @args
-        }
-        if ($LASTEXITCODE -eq 0) {
-            __zoxide_cd $result $true
-        }
-    }
-
-    Change-Directory
-}
-
-# Jump to a directory using interactive search.
-function global:__zoxide_zi {
-    $result = __zoxide_bin query -i -- @args
-    if ($LASTEXITCODE -eq 0) {
-        __zoxide_cd $result $true
-    }
-
-    Change-Directory
-}
-
-# =============================================================================
-#
-# Commands for zoxide. Disable these using --no-cmd.
-#
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
 Set-Alias -Name cd -Value __zoxide_z -Option AllScope -Scope Global -Force
 Set-Alias -Name cdi -Value __zoxide_zi -Option AllScope -Scope Global -Force
-#
-# =============================================================================
-#
-# To initialize zoxide, add this to your configuration (find it by running
-# `echo $profile` in PowerShell):
-#
-# Invoke-Expression (& { (zoxide init powershell | Out-String) })
-
-. $HOME\.config\powershell\env_profile.ps1
-
-
